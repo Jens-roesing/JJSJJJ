3a84f55fab6ca0c1480d24a30d3e942c7bee2d09
Jens Rösing <roesing.jens@gmx.de>
2025-01-24 22:24:50 +0100

    add menu to scene, with first buttons

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -1,16 +1,51 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    [SerializeField] private TextMeshProUGUI _headline;
+    [SerializeField] private Button _startButton;
+    [SerializeField] private Button _creditsButton;
+    [SerializeField] private Button _closeButton;
 
+    private void Awake()
+    {
+        _startButton.onClick.AddListener(StartGame);
+        _creditsButton.onClick.AddListener(OpenCredits);
+        _closeButton.onClick.AddListener(CloseGame);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        GameManager.GetInstance().NewGameState.AddListener(HandlePauseGameState);
+    }
+
+    private void HandlePauseGameState(GameManager.GameState state)
+    {
+        if (state == GameManager.GameState.Pause)
+            OpenPauseMenu();
+    }
+
+    private void StartGame()
+    {
+        Debug.Log("StartGame");
+    }
+
+    private void OpenCredits()
+    {
+        Debug.Log("OpenCredits");
+    }
+
+    private void CloseGame()
+    {
+        Debug.Log("CloseGame");
+        Application.Quit();
+    }
+
+    private void OpenPauseMenu()
+    {
+        Debug.Log("PauseGame");
     }
 }
