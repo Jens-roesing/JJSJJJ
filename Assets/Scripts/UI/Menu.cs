36ea63597fd2c373b6a9d4b1e2b2b563c491d149
Jens Rösing <roesing.jens@gmx.de>
2025-01-24 21:47:35 +0100

    fix in menu script

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -5,7 +5,7 @@ public class Menu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameManager.GetInstance().State
+
     }
 
     // Update is called once per frame
