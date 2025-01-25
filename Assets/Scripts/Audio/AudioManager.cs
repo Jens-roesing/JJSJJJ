6fa2ba895ecc15ba0c7b0bdebd58c87da448bfd7
Jens Rösing <roesing.jens@gmx.de>
2025-01-25 17:28:12 +0100

    add of audio sounds blop

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
@@ -5,9 +6,14 @@ public class AudioManager : MonoBehaviour
     private static AudioManager instance;
     [SerializeField] private AudioSource _menuSource;
     [SerializeField] private AudioSource _ingameSource;
+    [SerializeField] private AudioSource _effectSounds;
+    [SerializeField] private AudioSource _bubbleEffects;
     [SerializeField] private AudioClip[] audioClips;
+    [SerializeField] private AudioClip[] _bubbleClips;
+    [SerializeField] private float bubbleDelay = 0.1f;
     private MusicIntensity _musicIntensityState;
     private bool _menuOpen;
+    private int testDing;
 
     public enum MusicIntensity
     {
@@ -26,6 +32,21 @@ public class AudioManager : MonoBehaviour
     {
         PlayMenuMusic();
         GameManager.GetInstance().NewGameState.AddListener(HandleGameState);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            testDing++;
+            PlaySelectSound(testDing);
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            StartCoroutine(PlayBubblePopSounds(7));
+        }
+
     }
 
     private void HandleGameState(GameManager.GameState state)
@@ -54,6 +75,29 @@ public class AudioManager : MonoBehaviour
 
         if (!_ingameSource.isPlaying)
             _ingameSource.Play();
+    }
+
+    public void PlaySelectSound(int index)
+    {
+        _effectSounds.pitch = 1 + ((float)index / 10);
+        _effectSounds.Play();
+    }
+
+    public IEnumerator PlayBubblePopSounds(int bubbleCount)
+    {
+        int soundIndex = 0;
+        for (int i = 0; i < bubbleCount; i++)
+        {
+            yield return new WaitForSeconds(bubbleDelay);
+            if (soundIndex < _bubbleClips.Length - 1)
+                soundIndex++;
+            else
+                soundIndex = 0;
+            Debug.Log("Sound: " + soundIndex);
+            _bubbleEffects.PlayOneShot(_bubbleClips[soundIndex]);
+        }
+
+        yield return null;
     }
 
     public void SetMusicIntensity(MusicIntensity musicIntensity)
