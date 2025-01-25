55faaf33cb86f1c9af5b86eeeb5fd56597574e67
Juri Weber <43060805+Jomshanseat@users.noreply.github.com>
2025-01-25 18:13:16 +0100

    Slight Update & Restoration of Test Scene
    Bubble Raising still doesn't function, but I can at least test from within my test scene now

--- Assets/Scripts/Bubbles/BubblePopAnalyzer.cs
+++ Assets/Scripts/Bubbles/BubblePopAnalyzer.cs
@@ -8,23 +8,22 @@ public class BubblePopAnalyzer : MonoBehaviour
     public void CalculateResults(int p_bubblesPopped)
     {
         int damage = p_bubblesPopped;
-
-
-        if(EnemyHealth.GetInstance().RemoveLifepoints(damage))
-        {
-            Debug.Log("You Won, guy.");
-        }
+        if (EnemyHealth.GetInstance())
+            if (EnemyHealth.GetInstance().RemoveLifepoints(damage))
+            {
+                Debug.Log("You Won, guy.");
+            }
 
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
