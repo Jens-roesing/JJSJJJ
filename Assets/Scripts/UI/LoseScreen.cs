96db53e308750d9389ef8456af35782aff82466d
Jens Rösing <roesing.jens@gmx.de>
2025-01-26 14:00:17 +0100

    add of win and lose, scene restarts after 5 seconds

--- Assets/Scripts/UI/LoseScreen.cs
+++ Assets/Scripts/UI/LoseScreen.cs
@@ -1,11 +1,47 @@
-using UnityEngine;
 
-public class LoseScreen : MonoBehaviour
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Animation _loseAnimation;
+    [SerializeField] private TextMeshProUGUI loseText;
+    [SerializeField] private TextMeshProUGUI winText;
+
+    private void Start()
     {
-        
+        GameManager.GetInstance().NewGameState.AddListener(HandleGameState);
+    }
+
+    private void HandleGameState(GameManager.GameState State)
+    {
+        if (State == GameManager.GameState.Lose)
+            ShowLoseScreen();
+
+        if (State == GameManager.GameState.Win)
+            ShowLoseScreen();
+    }
+
+    private void ShowLoseScreen()
+    {
+        _loseAnimation.Play();
+        loseText.gameObject.SetActive(true);
+        StartCoroutine(RestartGame(5f));
+    }
+
+    private void ShowWinScreen()
+    {
+        Debug.Log("Show Lose Screen");
+        winText.gameObject.SetActive(true);
+        StartCoroutine(RestartGame(5f));
+    }
+
+    private IEnumerator RestartGame(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     // Update is called once per frame
