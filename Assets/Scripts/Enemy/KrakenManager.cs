1aaddcd2a87caf29bdadfdf1985146d4f37edcda
Jens Rösing <roesing.jens@gmx.de>
2025-01-26 14:51:46 +0100

    kraken now attacks

--- Assets/Scripts/Enemy/KrakenManager.cs
+++ Assets/Scripts/Enemy/KrakenManager.cs
@@ -1,16 +1,44 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class KrakenManager : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private Animator tentacle;
+    [SerializeField] private float inkDelay = 2f;
     private AudioManager audioManager;
+    private float nextAttackDelay = 0;
+    private float timer = 0;
 
     void Start()
     {
         audioManager = AudioManager.GetInstance();
         GameManager.GetInstance().NewGameState.AddListener(HandleGameState);
+        nextAttackDelay = UnityEngine.Random.Range(6, 10);
+    }
+
+    private void Update()
+    {
+        if (GameManager.GetInstance().State == GameManager.GameState.Playing)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= nextAttackDelay)
+            {
+                timer = 0;
+                nextAttackDelay = UnityEngine.Random.Range(6, 10);
+                Debug.Log($"next Attack in {nextAttackDelay} sec.");
+                StartCoroutine(PlayAttack());
+            }
+        }
+    }
+
+    private IEnumerator PlayAttack()
+    {
+        tentacle.SetTrigger("Attack");
+        yield return new WaitForSeconds(inkDelay);
+        BubbleManager.Instance.InkSplatter(UnityEngine.Random.Range(2, 5));
     }
 
     private void HandleGameState(GameManager.GameState state)
