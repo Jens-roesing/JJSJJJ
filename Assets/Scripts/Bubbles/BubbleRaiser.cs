b311869ac32eeaa29a214bf3c7b3a7cea9c879a4
Jens Rösing <roesing.jens@gmx.de>
2025-01-26 12:39:45 +0100

    made bubble raiser animation

--- Assets/Scripts/Bubbles/BubbleRaiser.cs
+++ Assets/Scripts/Bubbles/BubbleRaiser.cs
@@ -1,13 +1,19 @@
-using Unity.VisualScripting;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BubbleRaiser : MonoBehaviour
 {
     int count = 0;
     bool FullyDone = true;
+    private Dictionary<Bubble, Vector3> BubblesAnimations = new Dictionary<Bubble, Vector3>();
+    [SerializeField] private float animSpeed = 10;
+
     [ContextMenu("RaiseBubbles")]
     public void RaiseBubbles()
     {
+        BubblesAnimations = new Dictionary<Bubble, Vector3>();
         do
         {
             FullyDone = true;
@@ -29,20 +35,47 @@ public class BubbleRaiser : MonoBehaviour
                         continue;
                     FullyDone = false;
                     Bubble curBub = BubbleManager.Instance.Bubbles[i, j];
+
                     BubbleManager.Instance.Bubbles[i, j + 1] = curBub;
                     BubbleManager.Instance.Bubbles[i, j] = null;
                     curBub.BubblePos.y++;
+
+                    Vector3 targetPosition = new Vector3(curBub.transform.localPosition.x, curBub.transform.localPosition.y + BubbleManager.Instance.DistanceMod, curBub.transform.localPosition.z);
+                    if (BubblesAnimations.ContainsKey(curBub))
+                    {
+                        // Support for bubbles that get destroyed for lines from top to down
+                        Transform tempBubblePos = curBub.transform;
+                        tempBubblePos.localPosition = BubblesAnimations[curBub];
+                        Vector3 secondTargetPosition = new Vector3(tempBubblePos.transform.localPosition.x, tempBubblePos.transform.localPosition.y + BubbleManager.Instance.DistanceMod, tempBubblePos.transform.localPosition.z);
+                        BubblesAnimations[curBub] = secondTargetPosition;
+                    }
+                    else
+                        BubblesAnimations.Add(curBub, targetPosition);
+
                     //Replace with Animation Movement
-                    curBub.transform.localPosition = new Vector3(curBub.transform.localPosition.x, curBub.transform.localPosition.y + BubbleManager.Instance.DistanceMod, curBub.transform.localPosition.z);
+                    //curBub.transform.localPosition = new Vector3(curBub.transform.localPosition.x, curBub.transform.localPosition.y + BubbleManager.Instance.DistanceMod, curBub.transform.localPosition.z);
                 }
         } while (!FullyDone);
+
+        foreach (Bubble bubble in BubblesAnimations.Keys)
+        {
+            StartCoroutine(MoveBubble(bubble, BubblesAnimations[bubble]));
+        }
 
         count = 0;
     }
 
-
-
-
-
-
+    private IEnumerator MoveBubble(Bubble curbBubble, Vector3 targetPosition)
+    {
+        Vector3 startPosition = curbBubble.transform.localPosition;
+        Vector3 targetPositionOld = new Vector3(curbBubble.transform.localPosition.x, curbBubble.transform.localPosition.y + BubbleManager.Instance.DistanceMod, curbBubble.transform.localPosition.z);
+        float timer = 0;
+        while (timer < 1)
+        {
+            timer += Time.deltaTime * animSpeed;
+            curbBubble.transform.localPosition = Vector3.Lerp(startPosition, targetPosition, timer);
+            yield return null;
+        }
+        yield return null;
+    }
 }
